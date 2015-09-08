866b7e9b07b2d6d649834db0e224bc3b9a54739b
unknown <kabeerdarocker@gmail.com>
2015-09-08 21:25:51 +0545

    master page

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -5,7 +5,6 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>{{$title or 'Rastriya Banijya Bank - Social App'}}</title>
-    <link rel="stylesheet" href="/css/bootstrap.css" rel="stylesheet"/>
 	<link href="/css/main.css" rel="stylesheet">
 	<!-- Fonts -->
 	{{--<link href='//fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>--}}
@@ -25,53 +24,99 @@
 </style>
 </head>
 <body>
-<div class="container">
-	<nav class="navbar navbar-default">
-		<div class="container-fluid">
-			<div class="navbar-header">
-				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-					<span class="sr-only">Toggle Navigation</span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-				</button>
-				<a class="navbar-brand" href="/">Rastriya Banijya Bank</a>
-			</div>
+    <div id="wrapper">
+    	<nav class="navbar navbar-default">
+    		<div class="container">
+    			<div class="navbar-header">
+    				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#nav-collapse-open">
+    					<span class="sr-only">Toggle Navigation</span>
+    					<span class="icon-bar"></span>
+    					<span class="icon-bar"></span>
+    					<span class="icon-bar"></span>
+    				</button>
+    			</div>
 
-			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-				@if( Auth::check() )
-                    <ul class="nav navbar-nav">
-                        <li><a href="/notifications">Notifications</a></li>
-                        <li><a href="/conversations">Messages</a></li>
-                        <li><a href="/home">Home</a></li>
-                    </ul>
-                @endif
-
-                <ul class="nav navbar-nav navbar-right">
-					@if (Auth::guest())
-						<li><a href="{{ url('/auth/login') }}">Login</a></li>
-						{{-- <li><a href="{{ url('/auth/register') }}">Register</a></li> --}}
-					@else
-						<li class="dropdown">
-							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
-							<ul class="dropdown-menu" role="menu">
-                                <li><a href="/settings/profile">Edit Profile</a>
-                                <li><a href="/settings">Settings</a></li>
-                                <li><a href="/help">Help</a></li>
-                                <li><a href="/auth/logout">Logout</a></li>
-							</ul>
-						</li>
-					@endif
-				</ul>
-			</div>
-		</div>
-	</nav>
-    @yield('content')
+    			<div class="collapse navbar-collapse" id="nav-collapse-open">
+    				@if( Auth::check() )
+                        <ul class="nav navbar-nav">
+                            <li><a href="/home">Home</a></li>
+                        </ul>
+                        <form class="navbar-form navbar-left" role="search" action="/search">
+                            <div class="form-group">
+                               <input type="text" name="q" value="{{ Input::get('q') }}" class="form-control" placeholder="Search a Friend">
+                            </div>
+                        </form>
+                    @endif
+                    <ul class="nav navbar-nav pull-right">
+    					@if (Auth::guest())
+    						<li><a href="/login">Login</a></li>
+    						{{-- <li><a href="{{ url('/auth/register') }}">Register</a></li> --}}
+    					@else
+                            <li><a href="/notifications">Notifications</a></li>
+                            {{-- <li><a href="/conversations">Messages</a></li> --}}
+                            <li><a href="/friends">Friends</a></li>
+    						<li class="dropdown">
+    							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
+    							<ul class="dropdown-menu" role="menu">
+                                    <li><a href="/settings/profile">Edit Profile</a>
+                                    <li><a href="/settings">Settings</a></li>
+                                    <li><a href="/help">Help</a></li>
+                                    <li><a href="/auth/logout">Logout</a></li>
+    							</ul>
+    						</li>
+    					@endif
+    				</ul>
+    			</div>
+    		</div>
+    	</nav>
+        <div class="container main">
+            @yield('content')
+        </div>
+      </div>
 	<footer>
-        &copy; @if(date('Y') > $buildYear = 2015) $buildYear - @endif {{date('Y')}}
-        Rastriya Banijya Bank Limited.
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <h4 class="log">
+                        Social App
+                    </h4>
+                </div>
+                <div class="col-md-2">
+                    <ul>
+                        <li>UseFul links</li>
+                        <li>UseFul links</li>
+                        <li>UseFul links</li>
+                        <li>UseFul links</li>
+                        <li>UseFul links</li>
+                    </ul>
+                </div>
+                <div class="col-md-2">
+                    <ul>
+                        <li>UseFul links</li>
+                        <li>UseFul links</li>
+                        <li>UseFul links</li>
+                        <li>UseFul links</li>
+                        <li>UseFul links</li>
+                    </ul>
+                </div>
+                <div class="col-md-4">
+                    <ul>
+                        <li>UseFul links</li>
+                        <li>UseFul links</li>
+                        <li>UseFul links</li>
+                        <li>UseFul links</li>
+                        <li>UseFul links</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="footer__bottom">
+                <p class="pull-left">
+                    &copy; @if(date('Y') > $buildYear = 2015) $buildYear - @endif {{date('Y')}}
+                    Rastriya Banijya Bank Limited.
+                </p>
+            </div>
+        </div>
 	</footer>
-</div>
 @include('layouts.partials.script')
 @yield('footer')
 	<!-- Scripts -->
