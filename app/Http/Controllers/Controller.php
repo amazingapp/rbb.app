0f968dcc5a6d03055fcbb9a7efb32c837f52d828
unknown <kabeerdarocker@gmail.com>
2015-09-08 21:26:43 +0545

    Basecontroller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,8 +5,14 @@ namespace Banijya\Http\Controllers;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
-
+use View, Auth;
 abstract class Controller extends BaseController
 {
     use DispatchesJobs, ValidatesRequests;
+
+    public function __construct()
+    {
+        View::share('signedIn', Auth::user());
+        View::share('authUser', Auth::user());
+    }
 }
