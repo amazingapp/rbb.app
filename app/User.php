4e07b8d87db954af789469edc12a38e171681c9b
unknown <kabeerdarocker@gmail.com>
2015-09-08 21:27:00 +0545

    updated user model

--- app/User.php
+++ app/User.php
@@ -19,17 +19,180 @@ class User extends Model implements AuthenticatableContract, CanResetPasswordCon
      */
     protected $table = 'users';
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = ['name', 'email', 'password'];
+    protected $fillable = [
+                        'name', 'email', 'password',
+                        'employee_id', 'mobile',
+                        'dob','designation','current_branch',
+                        'login_count'
+                        ];
+
+        /**
+         * The attributes excluded from the model's JSON form.
+         *
+         * @var array
+         */
+        protected $hidden = ['password', 'remember_token','login_count','created_at','updated_at','avatar','settings'];
+
+        protected $casts = [
+                'settings' => 'json'
+        ];
 
     /**
-     * The attributes excluded from the model's JSON form.
-     *
-     * @var array
+     * A User can have many posts
+     * @return
      */
-    protected $hidden = ['password', 'remember_token'];
+    public function posts()
+    {
+        return $this->hasMany(Post::class);
+    }
+
+
+    /**
+     * User can have many comments
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
+
+    /**
+     * Get the activity timeline for the user
+     */
+    public function activity()
+    {
+        return $this->hasMany(Activity::class)
+                 // ->with(['subject.status'])
+                 ->latest();
+    }
+
+    /**
+     * Default behaviour for password setting
+     * @param $value
+     */
+    public function setPasswordAttribute( $value )
+    {
+        $this->attributes['password'] = bcrypt($value);
+    }
+
+    /**
+     * Get the friends of the current user
+     * @return
+     */
+    public function friendsOfMine()
+    {
+        return $this->belongsToMany(User::class, 'friends',
+             'user_id',
+             'friend_id'
+            );
+    }
+
+    /**
+     * Is the user is friend of other friend
+     * @return
+     */
+    public function friendOf()
+    {
+        return $this->belongsToMany(User::class,
+                'friends',
+                'friend_id',
+                'user_id');
+    }
+
+    /**
+     * List of Acceptec friends
+     * @return
+     */
+    public function friends()
+    {
+        return $this->friendsOfMine()->wherePivot('accepted',true)
+                ->get()->merge(
+                            $this->friendOf()->wherePivot('accepted', true)->get()
+                        );
+    }
+
+    /**
+     * Check if the user has friends request
+     * @return [type] [description]
+     */
+    public function friendsRequests()
+    {
+        return $this->friendsOfMine()->wherePivot('accepted',false);
+    }
+
+
+    public function friendRequestsPending()
+    {
+        return $this->friendOf()->wherePivot('accepted', false);
+    }
+
+    public function hasFriendRequestPending(User $user)
+    {
+        return (bool) $this
+                    ->friendRequestsPending()
+                    ->where('id', $user->id)->count();
+    }
+
+
+    public function hasFriendRequestReceived(User $user)
+    {
+        // dd($this->friendsRequests()->get()->where('id',1));
+        return (bool) $this->friendsRequests()
+                        ->get()
+                        ->where('id',$user->id) // we are querying the collection now
+                        ->count();
+    }
+
+
+    /**
+     * Add a user
+     * @param User $user
+     */
+    public function addFriend(User $user)
+    {
+        return $this->friendOf()->attach($user);
+    }
+
+    /**
+     * Delete User of any type, pending, requested, or accepted
+     * @return
+     */
+    public function deleteFriend(User $user)
+    {
+        $this->friendsOfMine()->detach($user); // basically both will detach the user
+        $this->friendOf()->detach($user);
+    }
+
+    public function acceptFriendRequest(User $user)
+    {
+        return $this->friendsRequests()->get()
+                    ->where('id', $user->id)
+                    ->first()
+                    ->pivot
+                    ->update([
+                            'accepted' => true
+                        ]);
+    }
+
+    /**
+     * Check to see if the given user is connected in any way possible
+     * @param      $user
+     * @return     boolean
+     */
+    public function isConnectedWith(User $user)
+    {
+          $user = $this->friendsOfMine()->wherePivot('friend_id',$user->id)
+                            ->get()->merge(
+                                $this->friendOf()->wherePivot('friend_id', $user->id)->get()
+                        );
+        return (bool) $user->count();
+    }
+
+
+    public function isFriendsWith(User $user)
+    {
+        return (bool) $this->friends()
+                        ->where('id', $user->id)
+                        ->count();
+    }
 }
