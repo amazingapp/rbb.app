22ca1c37cbf608f5d6ebd78b05f3d2c0b5a3474e
unknown <kabeerdarocker@gmail.com>
2015-09-10 17:13:08 +0545

    migration for images (aavatar)

---
+++ database/migrations/2015_09_09_231613_create_images_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateImagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('images', function(Blueprint $table){
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->string('name');
+            $table->string('caption')->nullable();
+            $table->string('path');
+            $table->string('thumbnail_path');
+            $table->string('icon_path');
+            $table->timestamps();
+        });    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+          Schema::drop('images');
+    }
+}
