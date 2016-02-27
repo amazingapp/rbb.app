d629dc4f36e71a10904f074b847abceafe1ea357
unknown <kabeerdarocker@gmail.com>
2016-02-27 10:57:16 +0545

    view indented

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -9,35 +9,35 @@
                     <div class="pull-left">
                         <img src="/{{ $user->aavatar->thumbnail_path}}" alt="Placeholder" style="width:50px;height:50px;" />
                     </div>
-        @include('posts.partials.delete-post')
-    <div class="media-body post-media-tooltip">
-       <span class="text single-post-title">
-           <strong>
-               <a href="{{route('user.profile',[$user->employee_id])}}" > {{ $user->name }}</a>
-           </strong>
-            <a href="{!! route('users.posts',[$user->employee_id, $post->id]) !!}">
-                <span class="text text-muted">{{ $post->created_at->diffForHumans() }}</span></a>
-           </a>
-        </span>
-        <p class="text single-post-body">
-            {{$post->body}}
-        </p>
-    </div>
-        @if( $signedIn )
-            <form action="{{route('posts.comments', [$post->id])}}" method="POST" class="form-comment">
-            <input type="hidden" value="{{$post->id}}" name="post_id">
-            {!! csrf_field() !!}
-            <small class="text-muted comment-helper">Hit Ctrl + Enter to leave a comment</small>
-            <div class="input-group post__comment" style="padding-top: 22px;">
+                    @include('posts.partials.delete-post')
+                    <div class="media-body post-media-tooltip">
+                       <span class="text single-post-title">
+                           <strong>
+                               <a href="{{route('user.profile',[$user->employee_id])}}" > {{ $user->name }}</a>
+                           </strong>
+                        <a href="{!! route('users.posts',[$user->employee_id, $post->id]) !!}">
+                            <span class="text text-muted">{{ $post->created_at->diffForHumans() }}</span></a>
+                       </a>
+                    </span>
+                    <p class="text single-post-body">
+                        {{$post->body}}
+                    </p>
+                </div>
+                    @if( $signedIn )
+                        <form action="{{route('posts.comments', [$post->id])}}" method="POST" class="form-comment">
+                        <input type="hidden" value="{{$post->id}}" name="post_id">
+                        {!! csrf_field() !!}
+                        <small class="text-muted comment-helper">Hit Ctrl + Enter to leave a comment</small>
+                        <div class="input-group post__comment" style="padding-top: 22px;">
 
-                <span class="input-group-addon" style="border:none;padding:0px;position:relative;">
-                  <img src="/{{$authAavatar->icon_path}}" alt="Placeholder" style="width:33px;height:33px;" />
-                </span>
-                <textarea name="body" class="form-control comment" aria-describedby="sizing-addon2" placeholder="Write a comment .." rows="1"></textarea>
-            </div>
-            </form>
-        @endif
-</article>
+                            <span class="input-group-addon" style="border:none;padding:0px;position:relative;">
+                              <img src="/{{$authAavatar->icon_path}}" alt="Placeholder" style="width:33px;height:33px;" />
+                            </span>
+                            <textarea name="body" class="form-control comment" aria-describedby="sizing-addon2" placeholder="Write a comment .." rows="1"></textarea>
+                        </div>
+                        </form>
+                    @endif
+            </article>
     @if($comments->count())
         @foreach( $comments as $comment)
             @include('posts.partials.comment')
